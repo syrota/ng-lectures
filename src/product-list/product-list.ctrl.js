c40e31e184135afc3401a3ab7686b4f60c06a095
jotunnjs <f9119114@yandex.ru>
2015-03-24 20:55:28 +0200

    new files

---
+++ src/product-list/product-list.ctrl.js
@@ -0,0 +1,14 @@
+angular.module('root', [])
+.controller('Phones', function(){
+
+		this.phonesBase = [
+			{'phonesName': 'HTC', price: 200, rating: 91},
+			{'phonesName': 'Lenovo' , price: 100, rating: 54},
+			{'phonesName': 'Ipont' , price: 500, rating: 89},
+			{'phonesName': 'Samsung' , price: 250, rating: 74},
+			{'phonesName': 'Nokia' , price: 100500, rating: 100},
+			{'phonesName': 'Fly' , price: 0, rating: 2},
+			{'phonesName': 'Shansung' , price: 73, rating: 64}
+			
+		];
+})
